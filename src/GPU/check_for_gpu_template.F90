d286954c6fccfdf9e5bbae8f4a0c40539d812fa2
Sundaresan <sundaresang@iisc.ac.in>
2024-04-16 14:32:20 +0530

    Warning messages disabled in case amd streams is not enabled during build. This is done since amd streams is currently not working in elpa.

--- src/GPU/check_for_gpu_template.F90
+++ src/GPU/check_for_gpu_template.F90
@@ -72,17 +72,17 @@
 #ifdef WITH_AMD_GPU_VERSION
 #ifndef WITH_GPU_STREAMS
 #ifdef ADDITIONAL_OBJECT_CODE
-        write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-        write(error_unit,*) "This does mean reduced performace!"
-        write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-        write(*,*) "This does mean reduced performace!"
+!        write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!        write(error_unit,*) "This does mean reduced performace!"
+!        write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!        write(*,*) "This does mean reduced performace!"
 #else /* ADDITIONAL_OBJECT_CODE */
         ! myid is given as argument
         if (myid .eq. 0) then
-          write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-          write(error_unit,*) "This does mean reduced performace!"
-          write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-          write(*,*) "This does mean reduced performace!"
+!          write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!          write(error_unit,*) "This does mean reduced performace!"
+!          write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!          write(*,*) "This does mean reduced performace!"
         endif
 #endif /* ADDITIONAL_OBJECT_CODE */
 #endif /* WITH_GPU_STREAMS */
@@ -668,17 +668,17 @@
 #ifdef WITH_AMD_GPU_VERSION
 #ifndef WITH_GPU_STREAMS
 #ifdef ADDITIONAL_OBJECT_CODE
-        write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-        write(error_unit,*) "This does mean reduced performace!"
-        write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-        write(*,*) "This does mean reduced performace!"
+!        write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!        write(error_unit,*) "This does mean reduced performace!"
+!        write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!        write(*,*) "This does mean reduced performace!"
 #else /* ADDITIONAL_OBJECT_CODE */
         ! myid is given as argument
         if (myid .eq. 0) then
-          write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-          write(error_unit,*) "This does mean reduced performace!"
-          write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
-          write(*,*) "This does mean reduced performace!"
+!          write(error_unit,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!          write(error_unit,*) "This does mean reduced performace!"
+!          write(*,*) "You use the AMD-GPUs without enabling amd-gpu streams at build time!"
+!          write(*,*) "This does mean reduced performace!"
         endif
 #endif /* ADDITIONAL_OBJECT_CODE */
 #endif /* WITH_GPU_STREAMS */
